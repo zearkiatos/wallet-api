82aea32c4d71958c6dea8eb4e50d998525ecdc38
zearkiatos <caprilespe@outlook.com>
2022-06-27 16:29:44 -0400

    MovementService Part II

--- src/services/MovementService.ts
+++ src/services/MovementService.ts
@@ -4,6 +4,7 @@ import Movement from "../repositories/domain/Movement";
 import { MovementCreateDTO } from "../DTOs/Movement";
 import MovementTypes from "../common/Enums/MovementTypes";
 import ApplicationException from "../common/Exceptions/ApplicationException";
+import Balance from "../repositories/domain/Balance";
 class MovementService {
   constructor(
     private readonly movementRepository: MovementRepository,
@@ -22,9 +23,36 @@ class MovementService {
     const balance = await this.balanceRepository.findByUserId(entry.userId);
 
     if (entry.type === MovementTypes.INCOME) {
+      this.income(entry, balance);
     } else if (entry.type === MovementTypes.OUTCOME) {
+      this.outcome(entry, balance);
     } else {
       throw new ApplicationException("Invalid movement yupe supplied");
+    }
+  }
+
+  public async income(entry: MovementCreateDTO, balance: Balance | null) {
+    if (!balance) {
+      await this.balanceRepository.store({
+        amount: entry.amount,
+        userId: entry.userId,
+      } as Balance);
+    } else {
+      balance.amount += entry.amount;
+      await this.balanceRepository.update(balance);
+    }
+
+    await this.movementRepository.store(entry as Movement);
+  }
+
+  public async outcome(entry: MovementCreateDTO, balance: Balance | null) {
+    if (!balance || balance.amount < entry.amount) {
+      throw new ApplicationException("User does not have enough balace.");
+    }
+    else {
+        balance.amount -= entry.amount;
+        await this.balanceRepository.update(balance);
+        awiat this.movementRepository.store(entry as Movement);
     }
   }
 }
